e1019357c45c97456217ea22383cab1b4f74c416
agascmur <agascmur@gmail.com>
2024-06-27 23:43:15 +0200

    init part 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,31 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import F
-from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
-from .models import Question, Choice
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-# Leave the rest of the views (detail, results, vote) unchanged
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+from .models import Choice, Question
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
